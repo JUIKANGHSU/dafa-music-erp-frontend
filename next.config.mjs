28b3b1cb9e764d37c0a3274b7b160ca80f12292f
徐瑞康 <juikang@MacBookPro.hitronhub.home>
2026-04-26 14:29:27 +0800

    Add Render deployment config and dynamic backend URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
+const BACKEND_URL = process.env.BACKEND_URL || "http://127.0.0.1:8000"
+
 const nextConfig = {
     async rewrites() {
         return [
             {
                 source: '/api/:path*/',
-                destination: 'http://127.0.0.1:8000/api/:path*',
+                destination: `${BACKEND_URL}/api/:path*`,
             },
             {
                 source: '/api/:path*',
-                destination: 'http://127.0.0.1:8000/api/:path*',
+                destination: `${BACKEND_URL}/api/:path*`,
             },
         ]
     },
 };
 
 export default nextConfig;
-
